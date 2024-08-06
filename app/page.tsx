2a32a6d6cf7003f00ccddc3344a11ed04ed2603d
Rafaellrx12 <rafaeloficial555@gmail.com>
2024-08-06 13:56:33 -0300

    feat: add header components ands banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-
-
-
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return  <Button>teste</Button>;
-};
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Rafael!</h2>
+        <p>Segunda-Feira, 05 de agosto.</p>
 
-export default Home;
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com Emiss Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
